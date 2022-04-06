67f1f78dbc58cd37d5d0d9c81b282907657b075d
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-06 16:18:59 +0100

    added link to artists to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,7 @@ function Nav() {
             <li><a class="active" href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#profile">Profile</a></li>
+            <li><a href="#profile">Artists</a></li>
           </ul>
       </nav>
     </div>
